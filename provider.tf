2016e1cbf9deebc529a4b235cc6aeb3c5c9e91c3
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-07-13 04:45:36 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "google" {
  region = "asia-south2-2"
  credentials = "${file("db-cicdpipeline-wave3-03bfb10085a9.json")}"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "cicd-action"
+    prefix = "terraform/state"
+  }
+}
