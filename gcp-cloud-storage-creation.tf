ad4295a6d4d60702cb542720ba548315f784c045
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-14 13:20:25 +0000

    Updated gcp-cloud-storage-creation.tf

--- gcp-cloud-storage-creation.tf
+++ gcp-cloud-storage-creation.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name          = "terraform_bucket_cicd"
+  name          = "terraform_bucket_1"
   location      = "asia-south2"
   force_destroy = true
 
