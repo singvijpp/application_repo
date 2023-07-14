e38a0213b0f895c57e1d139edf09609517330354
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-07-14 10:58:07 +0530

    Create VM create.tf

---
+++ VM create.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "n1-medium"
+  zone         = "us-central1-a"
+
+boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+ network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
