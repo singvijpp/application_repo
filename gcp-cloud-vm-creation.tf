5f802667fdc059460d655c06c5f34f415b22c7d1
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-18 16:13:33 +0000

    Update gcp-cloud-vm-creation.tf

--- gcp-cloud-vm-creation.tf
+++ gcp-cloud-vm-creation.tf
@@ -3,22 +3,18 @@ provider "google" {
   region      = "asia-south1"
   zone        ="asia-south1-a"
 }
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}
 resource "google_compute_instance" "vm_instance" {
-name = "terraform-instance2"
-machine_type = "f1-micro"
-zone = "us-central1-c"
-boot_disk {
-initialize_params {
-image = "centos-cloud/centos-7"
-}
-}
- network_interface {
-    network = "default"
-    access_config {
+    name         = "gcpwave3-vm"
+    machine_type = "f1-micro"
+  
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-9"
+      }
+    }    
+    network_interface {       
+      network = "default"
+      access_config {
+      }
     }
   }
-}
