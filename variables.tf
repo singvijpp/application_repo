7a646a53b05910d1883f71b56629da110c2be5e9
NikhilChougala <nikhilchougala@gmail.com>
2023-07-18 22:49:58 +0530

    region was added twice

--- variables.tf
+++ variables.tf
@@ -7,3 +7,37 @@ variable "region" {
     type = string
     default = "asia-east2"
 }
+
+variable "key_name" {
+
+  description = "Name of the KMS key"
+
+  type = string
+
+}
+
+variable "keyring_name" {
+
+  description = "Name of the KMS Keyring"
+
+  type = string
+
+}
+
+variable "algorithm" {
+
+  description = "Algorithm for the KMS key"
+
+
+  type = string
+
+}
+
+variable "rotation_period" {
+
+  description = "Time in seconds to rotate key"
+
+  type = string
+
+
+}
