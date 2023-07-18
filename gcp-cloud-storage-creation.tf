2dfe8fb927022fd7b1b00e51b012acb65d760e43
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-18 10:00:12 +0000

    Update gcp-cloud-storage-creation.tf
    commented

--- gcp-cloud-storage-creation.tf
+++ gcp-cloud-storage-creation.tf
@@ -1,4 +1,4 @@
-provider "google" {
+/* provider "google" {
   project     = "db-cicdpipeline-wave3"
   region      = "asia-south1"
   zone        ="asia-south1-a"
@@ -18,3 +18,4 @@ resource "google_compute_instance" "vm_instance" {
       }
     }
   }
+*/
