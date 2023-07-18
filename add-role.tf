612e140a188ad9479ffdc861ba1da268f436effb
gargswa1 <swapnil.gargade@tcs.com>
2023-07-18 13:49:02 +0000

    added and renamed file

---
+++ add-role.tf
@@ -0,0 +1,7 @@
+resource "google_project_iam_member" "project" {
+  project = "db-cicdpipeline-wave3"
+  role    = "roles/editor"
+  members = [
+    "service_account: ${{ secrets.garage_service_account }}"
+  ]
+}
