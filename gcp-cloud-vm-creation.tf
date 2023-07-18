98511e8253227df7f0851a44e9e41a20bfc937f7
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-18 14:39:35 +0000

    Update gcp-cloud-vm-creation.tf

--- gcp-cloud-vm-creation.tf
+++ gcp-cloud-vm-creation.tf
@@ -1,4 +1,4 @@
-resource "google_project_iam_binding" "project" {
+/*resource "google_project_iam_binding" "project" {
   project = "db-cicdpipeline-wave3"
   role    = "roles/editor"
   members = "service_account:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
@@ -22,4 +22,4 @@ resource "google_compute_instance" "vm_instance" {
       access_config {
       }
     }
-  }
+  } */
