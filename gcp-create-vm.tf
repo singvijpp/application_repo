fafa702c3d42e5c14deb068a1169e12fe72e3595
deshprac <138595507+deshprac@users.noreply.github.com>
2023-07-19 03:37:05 +0000

    Update gcp-create-vm.tf

--- gcp-create-vm.tf
+++ gcp-create-vm.tf
@@ -1,8 +1,6 @@
 resource "google_compute_instance" "vm_instance" {
   name		= "gcp-wave3-vm"
   machine_type	= "f1-micro"
-  region	= "us-central1"
-
   boot_disk {
     initialize_params {
 	image = "debian-cloud/debian-9"
