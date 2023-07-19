0f65448a8720b87988b7f8cd42556e9225cc986d
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-19 16:49:07 +0000

    created network

--- gcp-cloud-vm-creation.tf
+++ gcp-cloud-vm-creation.tf
@@ -3,6 +3,10 @@ provider "google" {
   region      = "asia-south1"
   zone        ="asia-south1-a"
 }
+resource "google_compute_network" "default" {
+  name                    = "wave3-network"
+  auto_create_subnetworks = "false"
+}
 resource "google_compute_instance" "vm_instance" {
     name         = "gcpwave3-vm"
     machine_type = "f1-micro"
@@ -12,7 +16,7 @@ resource "google_compute_instance" "vm_instance" {
         image = "centos-cloud/centos-7"
       }
     }    
-    network_interface {       
-      network = "wave-3"
-    }
+    network_interface {
+    network = "${google_compute_network.default.name}"
+  }
   }
