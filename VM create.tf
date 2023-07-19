40c9c10aa8ffc01825868353be900f24b850b704
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-07-19 09:57:50 +0530

    Update VM create.tf

--- VM create.tf
+++ VM create.tf
@@ -1,21 +1,15 @@
-resource "google_compute_instance" "VM_Test" {
-  name         = "VMtest"
+resource "google_compute_instance" "KMSVM" {
+  name         = "KMS-VM"
   machine_type = "e2-medium"
   zone         = "asia-south2-a"
 
 boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
     }
   }
  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
+    network = "default" // This enable Private Internal IP  
+    access_config {} // This enable Ephemeral public IP  
   }
 }
