bbdddffbf1cf11140ecdfda55cdad90ef49f118e
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-19 06:43:41 +0000

    Update add-role.tf

--- add-role.tf
+++ add-role.tf
@@ -1,5 +1,5 @@
-resource "google_project_iam_member" "project" {
+/*resource "google_project_iam_member" "project" {
   project = "db-cicdpipeline-wave3"
   role    = "roles/editor"
   member  = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
-}
+}*/
