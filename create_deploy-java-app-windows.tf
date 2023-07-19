f113dda3317c30cd9bbd4017d0b8ff1ec5e74fa6
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-07-19 06:08:38 +0000

    Update create_deploy-java-app-windows.tf

--- create_deploy-java-app-windows.tf
+++ create_deploy-java-app-windows.tf
@@ -7,13 +7,6 @@ resource "google_compute_instance" "wave3_java_app_windows_instance" {
       image = "windows-10"
     }
   }
-
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    git clone https://github.com/shashirajraja/onlinebookstore.git
-    cd onlinebookstore
-    # Add additional commands for building and deploying the Java application
-  
   network_interface {
     network = "default"
     }
