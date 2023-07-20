b7cc30589516cd09e49addd4ac29c23567471932
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-07-20 03:05:33 +0000

    Update wave3-cloud-kms.tf

--- wave3-cloud-kms.tf
+++ wave3-cloud-kms.tf
@@ -2,15 +2,30 @@ resource "google_kms_key_ring" "wave3_kms_key_ring" {
   name     = "wave3-key-ring"
   location = "global"
 }
+resource "google_storage_bucket" "bucket" {
+  name          = "terraform_bucket_cicd"
+  location      = "asia-south2"
+  force_destroy = true
 
-resource "google_kms_crypto_key" "wave3_kms_crypto_key" {
-  name            = "wave3-crypto-key"
-  key_ring        = google_kms_key_ring.wave3_kms_key_ring
-  rotation_period = "100000s"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+encryption {
+    default_kms_key_name = google_kms_crypto_key.example_crypto_key.self_link
+  }  
+action {
+      type = "Delete"
+    }
+  }
 }
 
-resource "google_storage_bucket_iam_member" "wave3_kms_bucket_iam_member" {
-  bucket = google_storage_bucket.bucket.terraform_bucket_cicd
-  role   = "roles/storage.objectViewer"
-  member = "serviceAccount:${google_kms_crypto_key.wave3_kms_crypto_key}"
+# Create a KMS crypto key
+resource "google_kms_crypto_key" "example_crypto_key" {
+  name      = "example-crypto-key"
+  key_ring  = google_kms_key_ring.wave3_kms_key_ring.self_link
 }
