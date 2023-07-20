3a982b4ca6f3a76d45075738c9dcb7f3cb7d1124
deshprac <138595507+deshprac@users.noreply.github.com>
2023-07-20 10:43:34 +0000

    Update gcp-create-kms.tf

--- gcp-create-kms.tf
+++ gcp-create-kms.tf
@@ -1,11 +1,24 @@
 
-resource "google_kms_key_ring" "default" {
+provider "google" {
 
-  name = "gcp-wave3-ring"
+project = "db-cicdpipeline-wave3"
+
+region = "asia-south2-a"
+}
+
+resource "google_kms_key_ring" "gcp_wave3_ring" {
+
+  name = "gcp_wave3_ring"
 
   location = "asia-east2"
 
 }
+
+resource "google_kms_crypto_key" "gcp_crypto_key_kms" {
+
+  name = "gcp_crypto_key_kms"
+
+  key_ring = "google_kms_key_ring.gcp_wave3_ring"
 
 
   version_template {
@@ -13,6 +26,6 @@ resource "google_kms_key_ring" "default" {
     algorithm = "GOOGLE_SYMMETRIC_ENCRYPTION"
 
   }
-
+}
 
 
