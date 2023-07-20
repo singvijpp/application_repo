219c32482780e33fda13fba34d3d81c06dd44183
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-07-20 02:53:33 +0000

    Update wave3-cloud-kms.tf

--- wave3-cloud-kms.tf
+++ wave3-cloud-kms.tf
@@ -10,7 +10,7 @@ resource "google_kms_crypto_key" "wave3_kms_crypto_key" {
 }
 
 resource "google_storage_bucket_iam_member" "wave3_kms_bucket_iam_member" {
-  bucket = google_storage_bucket.wave3_cicd_bucket
+  bucket = google_storage_bucket.terraform_bucket_cicd
   role   = "roles/storage.objectViewer"
-  member = "serviceAccount:${google_kms_crypto_key.wave3_kms_crypto_key.primary_key_ring}"
+  member = "serviceAccount:${google_kms_crypto_key.wave3_kms_crypto_key}"
 }
