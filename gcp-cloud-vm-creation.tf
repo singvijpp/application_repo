a73c2ce572c3082cb506b8cef56e1da281e885b2
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-20 10:54:26 +0000

    Update gcp-cloud-vm-creation.tf

--- gcp-cloud-vm-creation.tf
+++ gcp-cloud-vm-creation.tf
@@ -1,19 +1,9 @@
-/*provider "google" {
+provider "google" {
   project     = "db-cicdpipeline-wave3"
   region      = "asia-south2"
   zone        ="asia-south2-a"
-}*/
-resource "google_compute_firewall" "wave3-firewall" {
-  name    = "firewall-externalssh-wave3"
-  network = "terraform-network"
-  allow {
-    protocol = "ssh"
-    ports    = ["22"]
-  }
-source_ranges = ["0.0.0.0/0"] 
-  target_tags   = ["externalssh-wave3"]
 }
-/*resource "google_compute_firewall" "allow-ssh" {
+resource "google_compute_firewall" "allow-ssh" {
   name = "allow-ssh"
   network = "wave-3"
   allow {
@@ -22,7 +12,7 @@ source_ranges = ["0.0.0.0/0"]
   }
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["gcpwave3-linux-vm"]
-}*/
+}
 /*resource "google_compute_instance" "vm_instance" {
     name         = "gcpwave3-linux-vm"
     machine_type = "f1-micro"
