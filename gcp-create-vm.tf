f59a3c0e27ef8865eaac4b0e66721910f4ad18cb
deshprac <138595507+deshprac@users.noreply.github.com>
2023-07-20 06:08:06 +0000

    Update gcp-create-vm.tf
    updated zone

--- gcp-create-vm.tf
+++ gcp-create-vm.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   name		= "gcp-wave3-vm"
   machine_type	= "f1-micro"
-  zone         = "us-central1-a"
+  zone         = "asia-south2-a"
   boot_disk {
     initialize_params {
 	image = "debian-cloud/debian-11"
