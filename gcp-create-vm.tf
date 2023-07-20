602d671c19c8289a1b39836fff61b4064de6bc84
deshprac <138595507+deshprac@users.noreply.github.com>
2023-07-20 06:56:27 +0000

    Update gcp-create-vm.tf

--- gcp-create-vm.tf
+++ gcp-create-vm.tf
@@ -10,7 +10,7 @@ resource "google_compute_instance" "default" {
 
    network_interface {
     network = "wave-3"
-    subnetwork = "CUSTOM"
+    subnetwork = "asia-south-1"
     access_config {
     
     }
