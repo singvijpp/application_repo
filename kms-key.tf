98d2b4bd55a4ae5ed4730b8d56f0b9d0047684f3
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-07-21 05:25:24 +0000

    Update kms-key.tf

--- kms-key.tf
+++ kms-key.tf
@@ -1,16 +1,17 @@
 resource "google_kms_key_ring" "kms_key_ring" {
   name     = "key-ring"
-  location = "asia-east2"
+  location = "asia-south2"
 }
 
 resource "google_kms_crypto_key" "kms_crypto_key" {
   name            = "crypto-key"
-  key_ring        = google_kms_key_ring.kms_key_ring.self_link
+  key_ring        = google_kms_key_ring.kms_key_ring.id
   rotation_period = "2592000s"
+
 }
 
 resource "google_kms_crypto_key_iam_binding" "crypto_key" {
-  crypto_key_id = google_kms_crypto_key.key.id
+  crypto_key_id = google_kms_crypto_key.kms_crypto_key.id
   role          = "roles/cloudkms.cryptoKeyEncrypter"
 
   members = [
