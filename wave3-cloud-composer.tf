1db3275f4d9bcd624892a681f844a9a6eb7a0402
vijay.psingh <vijay.psingh2@tcs.com>
2023-07-25 17:54:35 +0000

    adding composer code

---
+++ wave3-cloud-composer.tf
@@ -0,0 +1,26 @@
+resource "google_composer_environment" "test" {
+  name   = "example-composer-env"
+  region = "asia_south2"
+  config {
+    node_count = 4
+
+    node_config {
+      zone         = "us-central1-a"
+      machine_type = "n1-standard-1"
+
+      service_account = google_service_account.test.name
+    }
+
+    database_config {
+      machine_type = "db-n1-standard-2"
+    }
+
+    web_server_config {
+      machine_type = "composer-n1-webserver-2"
+    }
+  }
+}
+resource "google_service_account" "test" {
+  account_id   = "composer-env-account"
+  display_name = "Test Service Account for Composer Environment"
+}
