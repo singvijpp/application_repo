0c99c0f5b6d1c07b6635b01d9a92df714c11ef01
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-26 13:48:42 +0000

    Update gcp-cloud-vm-creation.tf

--- gcp-cloud-vm-creation.tf
+++ gcp-cloud-vm-creation.tf
@@ -1,8 +1,8 @@
-/*provider "google" {
+provider "google" {
   project     = "db-cicdpipeline-wave3"
   region      = "asia-south2"
   zone        ="asia-south2-a"
-}*/
+}
 /*resource "google_compute_firewall" "allow-ssh" {
   name = "allow-ssh"
   network = "wave-3"
@@ -14,7 +14,7 @@ disabled        = false
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["gcpwave3-linux-vm"]
 }*/
-/* resource "google_compute_instance" "vm_instance" {
+ resource "google_compute_instance" "vm_instance" {
     name         = "sonarqube-linux-vm"
     machine_type = "f1-micro"
     tags = ["sonarqube"]
@@ -29,4 +29,4 @@ disabled        = false
       subnetwork = "asia-south-1"
     access_config {}
   }
-  }*/
+  }
