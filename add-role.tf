c9a5a763910e772744e11f7f4672c4f6d21292c2
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-26 13:49:16 +0000

    Update add-role.tf

--- add-role.tf
+++ add-role.tf
@@ -1,10 +1,10 @@
-/*resource "google_project_iam_member" "project" {
+resource "google_project_iam_member" "project" {
   project = "db-cicdpipeline-wave3"
   role    = "roles/editor"
   member  = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
-}*/
-/* resource "google_project_iam_member" "tunnerole" {
+}
+ resource "google_project_iam_member" "tunnerole" {
   project = "db-cicdpipeline-wave3"
   role     = "roles/iap.tunnelResourceAccessor"
   member   = "user:swapnil.gargade@tcs.com"
-}*/
+}
