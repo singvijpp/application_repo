1efbfd474b6062c5716e31d0418b7e0efd350521
deshprac <138595507+deshprac@users.noreply.github.com>
2023-07-26 05:39:45 +0000

    Update addroles.tf

--- addroles.tf
+++ addroles.tf
@@ -1,5 +1,5 @@
-resource "google_project_iam_member" "associate" {
+/* resource "google_project_iam_member" "associate" {
   project = "db-cicdpipeline-wave3"
   role     = "roles/iap.tunnelResourceAccessor"
   member   = "user:pm.deshpande@tcs.com"
-}
+} */
