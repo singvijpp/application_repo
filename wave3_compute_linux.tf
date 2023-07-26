e8eee49a8629e961845811f616a2a2d809e962fe
vijay.psingh <vijay.psingh2@tcs.com>
2023-07-26 09:41:52 +0000

    added custom properties of cloud composer

--- wave3_compute_linux.tf
+++ wave3_compute_linux.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "wave3_compute_linux_instance" {
   name         = "wave3-compute-linux-instance"
   machine_type = "n1-standard-1"
-  location      = "asia-south2"
+  zone      = "asia-south2-a"
   boot_disk {
     initialize_params {
       image = "ubuntu-latest"
--- wave3_compute_windows_vm.tf
+++ wave3_compute_windows_vm.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "wave3_compute_windows_instance" {
   name         = "wave3-compute-windows-instance"
   machine_type = "n1-standard-1"
-  location      = "asia-south2"
+  zone      = "asia-south2-a"
 
   boot_disk {
     initialize_params {
