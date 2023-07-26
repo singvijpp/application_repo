48a463a765aa404988203174daa4aef358ce8fe7
Siddhant041198 <138601572+Siddhant041198@users.noreply.github.com>
2023-07-26 04:36:05 +0000

    Create Cloud Composer.tf

---
+++ Cloud Composer.tf
@@ -0,0 +1,43 @@
+resource "google_composer_environment" "test" {
+  name   = "example-composer-env-tf-c2"
+  region = "us-central1"
+  config {
+
+    software_config {
+      image_version = "composer-2-airflow-2"
+    }
+
+    workloads_config {
+      scheduler {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        count      = 1
+      }
+      web_server {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+      }
+      worker {
+        cpu = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        min_count  = 1
+        max_count  = 3
+      }
+
+
+    }
+    environment_size = "ENVIRONMENT_SIZE_SMALL"
+    node_config {
+      network    = google_compute_network.test.id
+      subnetwork = google_compute_subnetwork.test.id
+      service_account = google_service_account.test.name
+    }
+  }
+}
+resource "google_service_account" "test" {
+  account_id   = "composer-env-account"
+  display_name = "Test Service Account for Composer Environment"
+}
