0e99aba45d2910c6ab4b9793d6f145be7d259313
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-27 15:40:51 +0000

    Update add-role.tf

--- add-role.tf
+++ add-role.tf
@@ -8,12 +8,13 @@
   role     = "roles/iap.tunnelResourceAccessor"
   member   = "user:swapnil.gargade@tcs.com"
 }*/
-resource "google_compute_firewall" "sonarqube" {
-  name = "sonarqube"
+resource "google_compute_firewall" "sonarqube1" {
+  name = "sonarqube1"
   network = "wave-3"
   allow {
     protocol = "tcp"
-    ports    = ["9000"]
+    ports    = ["8787"]
   }
 target_tags = ["sonarqube"]
+source_ranges = ["0.0.0.0/0"]
 }
