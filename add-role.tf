cdd82dd5c3cdfcfa993943f0dbb41c3319e9c53d
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-27 08:56:39 +0000

    Update add-role.tf

--- add-role.tf
+++ add-role.tf
@@ -1,4 +1,4 @@
-/*resource "google_project_iam_member" "project" {
+resource "google_project_iam_member" "project" {
   project = "db-cicdpipeline-wave3"
   role    = "roles/editor"
   member  = "serviceAccount:cicd-wave3-serviceaccot@db-cicdpipeline-wave3.iam.gserviceaccount.com"
@@ -7,7 +7,7 @@
   project = "db-cicdpipeline-wave3"
   role     = "roles/iap.tunnelResourceAccessor"
   member   = "user:swapnil.gargade@tcs.com"
-}*/
+}
 resource "google_compute_firewall" "sonarqube" {
   name = "sonarqube"
   network = "wave-3"
