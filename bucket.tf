6cde4161295491e0fa44e1e78d4f3a7c7bcdaa07
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-27 09:03:35 +0000

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -2,8 +2,8 @@
   project     = "db-cicdpipeline-wave3"
   region      = "asia-south2"
   zone        ="asia-south2-a"
-}*/
+}
  resource "google_storage_bucket" "static" {
   name          = "gcp_state_cicd"
   location      = "asia-south2"
-}
+}*/
