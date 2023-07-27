e771a5021a19b9f2ed15d4457b4f99e16d984b2e
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-27 09:18:14 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project     = "db-cicdpipeline-wave3"
+  region      = "asia-south2"
+  zone        ="asia-south2-a"
+}
 terraform {
   backend "gcs" {
     bucket = "gcp_state_cicd"
