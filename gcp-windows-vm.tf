20cf7561520bd90eb5086841bc4fe72e3a55c3f2
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-07-27 08:59:06 +0000

    Update gcp-windows-vm.tf

--- gcp-windows-vm.tf
+++ gcp-windows-vm.tf
@@ -2,9 +2,9 @@
   project     = "db-cicdpipeline-wave3"
   region      = "asia-south2"
   zone        ="asia-south2-a"
-}
+}*/
 resource "google_compute_instance" "vm_instance" {
-    name         = "gcpwave3-windows-vm"
+    name         = "wave3-windows-vm"
     machine_type = "n1-standard-1"
     
     boot_disk {
@@ -17,4 +17,4 @@ resource "google_compute_instance" "vm_instance" {
       subnetwork = "asia-south-1"
     access_config {}
   }
-  }*/
+  }
