2558f3a6502a1bac2f808e047d044a8b953d4b20
vijay.psingh <vijay.psingh2@tcs.com>
2023-07-28 05:19:49 +0000

    added role given to cloud composer service account

--- wave3-cloud-composer.tf
+++ wave3-cloud-composer.tf
@@ -1,32 +1,42 @@
-resource "google_service_account" "composer_env_sa" {
-  project      = data.google_project.project.project_id
-  account_id   = "composer-worker-sa"
-  display_name = "Test Service Account for Composer Environment deployment "
+#   Usage:
+#       1. Select or create a new project that you will use to create the resources
+#       2. Set up your environment and apply the configuration using basic Terraform commands: https://cloud.google.com/docs/terraform/basic-commands
+#
+#   The script provisions the following resources in the project:
+#  	- Creates a VPC network and a subnetwork
+#	- Creates a Cloud Composer environment
+#	- Creates a Composer Service Agent account
+#	- Grants the Cloud Composer v2 API Service Agent Extension role and the Composer Worker role to the Composer Service Agent account
+#	- Creates a new Cloud Storage bucket
+#	- Creates a new Cloud Function and deploys the function source code from the pubsub_function.zip file to the Cloud Storage bucket
+#	- Uploads the example DAG source code from the specified file into the Cloud Composer bucket
+
+#
+
+
+
+resource "google_project_service" "composer" {
+  project = "db-cicdpipeline-wave3"
+  service = "composer.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+  disable_on_destroy         = false
 }
 
-resource "google_project_service_identity" "composer_sa" {
-  provider = google-beta
-  project  = data.google_project.project.project_id
-  service  = "composer.googleapis.com"
-}
-
-resource "google_project_iam_member" "composer_worker" {
-  project = data.google_project.project.project_id
-  role    = "roles/composer.worker"
-  member  = "serviceAccount:${google_service_account.composer_env_sa.email}"
-}
-
-resource "google_service_account_iam_member" "custom_service_account" {
-  provider           = google-beta
-  service_account_id = google_service_account.composer_env_sa.id
-  role               = "roles/composer.ServiceAgentV2Ext"
-  member             = "serviceAccount:${google_project_service_identity.composer_sa.email}"
-}
-
-resource "google_composer_environment" "example_environment" {
-  provider = google-beta
-  name = "wave3-cicd-composer-env"
-
+###############################
+#                             #
+# Create Composer environment #
+#                             #
+###############################
+resource "google_composer_environment" "new_composer_env" {
+  name    = "composer-environment"
+  region  = "asia-south2"
+  project = "db-cicdpipeline-wave3"
   config {
 
     software_config {
@@ -51,12 +61,45 @@ resource "google_composer_environment" "example_environment" {
         min_count  = 1
         max_count  = 3
       }
+
+
     }
     environment_size = "ENVIRONMENT_SIZE_SMALL"
 
     node_config {
-	  service_account = google_service_account.composer_env_sa.email
+      service_account = google_service_account.composer_env_sa.email
     }
-
   }
+}
+
+
+#########################
+#                       #
+# Creates IAM resources #
+#                       #
+#########################
+# Comment this section to use an existing service account
+resource "google_service_account" "composer_env_sa" {
+  project      = "db-cicdpipeline-wave3"
+  account_id   = "cicd-wave3-serviceaccot"
+  display_name = "Test Service Account for Composer Environment deployment "
+}
+
+resource "google_project_service_identity" "composer_sa" {
+  provider = google-beta
+  project  = "db-cicdpipeline-wave3"
+  service  = "composer.googleapis.com"
+}
+
+resource "google_project_iam_member" "composer_worker" {
+  project = "db-cicdpipeline-wave3"
+  role    = "roles/composer.worker"
+  member  = "serviceAccount:${google_service_account.composer_env_sa.email}"
+}
+
+resource "google_service_account_iam_member" "custom_service_account" {
+  provider           = google-beta
+  service_account_id = google_service_account.composer_env_sa.id
+  role               = "roles/composer.ServiceAgentV2Ext"
+  member             = "serviceAccount:${google_project_service_identity.composer_sa.email}"
 }
