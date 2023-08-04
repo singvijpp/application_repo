ac76baa83f50e8af164936a7f1701c8f40162db0
gargswa1 <138752260+gargswa1@users.noreply.github.com>
2023-08-04 07:42:02 +0000

    Update gcp-cloud-vm-creation.tf

--- gcp-cloud-vm-creation.tf
+++ gcp-cloud-vm-creation.tf
@@ -8,9 +8,9 @@
 disabled        = false
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["gcpwave3-linux-vm"]
-}*/
+}
  resource "google_compute_instance" "vm_instance" {
-    name         = "test-linux-vm"
+    name         = "sonarqube-linux-vm"
     machine_type = "f1-micro"
     tags = ["sonarqube"]
   
@@ -24,4 +24,4 @@ disabled        = false
       subnetwork = "asia-south-1"
     access_config {}
   }
-  }
+  }*/
