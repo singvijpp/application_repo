813b9eee1abbd34bd3f62769aec6df3cfdb12fa6
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-08-06 11:42:01 +0000

    Update process_terraform.py

--- process_terraform.py
+++ process_terraform.py
@@ -7,8 +7,8 @@ def process_file(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
         response = openai.Completion.create(
-            engine="gpt-3.5-turbo", 
-            messages=[
+            engine ="gpt-3.5-turbo", 
+            messages = [
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": content}
                 ]
