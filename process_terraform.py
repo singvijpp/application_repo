d2f76d5b0e0f795d3607dcf121a76a0a9822650a
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-08-06 10:33:08 +0000

    Create process_terraform.py

---
+++ process_terraform.py
@@ -0,0 +1,20 @@
+import openai
+import os
+import glob
+
+openai.api_key = secrets.OPENAI_API_KEY  # Assuming it's set as an environment variable
+
+def process_file(file_path):
+    with open(file_path, 'r') as f:
+        content = f.read()
+        response = openai.Completion.create(
+            engine="gpt-4.0-turbo", 
+            prompt=f"Generate documentation for this Terraform code: {content}",  
+            max_tokens=500
+        )
+        print(f"Documentation for {file_path}:")
+        print(response.choices[0].text.strip())
+
+# Process all Terraform files in the repository
+for file in glob.glob("**/*.tf", recursive=True):
+    process_file(file)
