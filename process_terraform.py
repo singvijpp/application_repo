21d5c6f02c5200407f5ed61af2a6fed4c3ee240c
singvijpp <138557480+singvijpp@users.noreply.github.com>
2023-08-06 11:54:21 +0000

    Update process_terraform.py

--- process_terraform.py
+++ process_terraform.py
@@ -6,7 +6,7 @@ openai.api_key = os.environ['OPENAI_API_KEY']  # Assuming it's set as an environ
 def process_file(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
-        response = openai.Completion.create(
+        response = openai.ChatCompletion.create(
             engine="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
