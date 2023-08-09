b1b49a0ac9d546b62a52e596edf90ddf4d0450cc
vijay.psingh <vijay.psingh2@tcs.com>
2023-08-09 12:14:27 +0000

    giving cloud sql for composer nvironment sa

--- wave3-cloud-composer.tf
+++ wave3-cloud-composer.tf
@@ -36,6 +36,28 @@ resource "google_composer_environment" "new_composer_env" {
     software_config {
       image_version = "composer-2-airflow-2"
     }
+    workloads_config {
+      scheduler {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        count      = 1
+      }
+      web_server {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+      }
+      worker {
+        cpu        = 0.5
+        memory_gb  = 1.875
+        storage_gb = 1
+        min_count  = 1
+        max_count  = 3
+      }
+
+
+    }
     environment_size = "ENVIRONMENT_SIZE_SMALL"
 
 	  node_config {
